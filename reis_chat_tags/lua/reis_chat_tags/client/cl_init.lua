017e74c2b1bbb8f9ad760d47ba772a814b4d0e08
Reispfannenfresser <34158734+Reispfannenfresser@users.noreply.github.com>
2026-09-17 23:08:36 +0200

    added some more settings to configure in which chat messages tags should appear

--- reis_chat_tags/lua/reis_chat_tags/client/cl_init.lua
+++ reis_chat_tags/lua/reis_chat_tags/client/cl_init.lua
@@ -2,10 +2,12 @@ require("pacoman")
 
 reis_chat_tags = {}
 reis_chat_tags.setting_namespace = pacoman.client_settings:AddChild("chat_tags")
+local player_messages_only = reis_chat_tags.setting_namespace:AddSetting("player_messages_only", pacoman.TYPE_BOOLEAN, true, "If set to true, it will only add chat-tags in player written messages.")
+local enabled = reis_chat_tags.setting_namespace:AddSetting("enabled", pacoman.TYPE_BOOLEAN, true, "Will only display tags when set to true.")
 
-local GMOnPlayerChat
 local ChatAddText
-local prefix = {}
+local GMOnPlayerChat
+local is_player_message = false
 
 hook.Add("RCT_GetChatTag", "GetChatTag", function(ply)
     return {}
@@ -16,24 +18,44 @@ local function isplayer(value)
 end
 
 local function AddText(...)
+    if(not enabled:GetActiveValue()) then
+        ChatAddText(...)
+        is_player_message = false
+        return
+    end
+    if (player_messages_only:GetActiveValue() and not is_player_message) then
+        ChatAddText(...)
+        return
+    end
+
     local text = {...}
     local args = {}
-    local index = 0
 
     for i = 1, #text do
-        index = i
         if(isplayer(text[i])) then
             table.Add(args, hook.Run("RCT_GetChatTag", text[i]))
         end
         table.insert(args, text[i])
     end
 
+    is_player_message = false
     ChatAddText(unpack(args))
+end
+
+local function OnPlayerChat(self, ply, strText, bTeam, bDead)
+    is_player_message = true
+
+    if GMOnPlayerChat(self, ply, strText, bTeam, bDead) then
+        return true
+    end
 end
 
 hook.Add("InitPostEntity", "RCT_InitPostEntity", function()
     ChatAddText = chat.AddText
     chat.AddText = AddText
+
+    GMOnPlayerChat = gmod.GetGamemode().OnPlayerChat
+    gmod.GetGamemode().OnPlayerChat = OnPlayerChat
 
     if(ulx) then
 	    include("reis_chat_tags/client/cl_ulx.lua")
